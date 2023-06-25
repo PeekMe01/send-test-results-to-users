a71f1c3e8b8bc119978bc7732523c2b240655ca5
PeekMe01 <ralphdaher6@gmail.com>
2023-06-25 09:00:49 +0300

    Update server.js

--- back-end/server.js
+++ back-end/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
 app.use(cors());
-
+app.use(express.json());
 
 // put your database info
 const db = mysql.createConnection({
@@ -18,14 +19,41 @@ app.get('/', (re, res)=> {
     return res.json("From Backend Side");
 })
 
-// here is where you fetch things
-app.get('/name it', (req, res)=> {
-    const sql = 'SQL statement'
-    db.query(sql, (err, data)=> {
-        if(err) return res.json(err);
-        return res.json(data);
-    })
-})
+// The signin part, not your typical one, i love chatgpt
+app.post('/signin', (req, res)=> {
+    const { username, password } = req.body;
+    const sql = 'SELECT id FROM test_result_users WHERE username = ?';
+
+    if (!username || !password) {
+        return res.status(400).json({ error: 'All fields are required.' });
+    }
+
+    db.query(sql, [username], (error, results) => {
+        if (error) {
+          console.error('Error:', error);
+          return res.status(500).json({ error: 'Internal server error.' });
+        }
+        if (results.length > 0) {
+            return res.status(409).json({ error: 'User already exists.' });
+        }
+
+        const newUser = {
+            _username: username,
+            _password: password,
+            _permission: false
+        };
+
+        db.query('INSERT INTO test_result_users(username,password,permission) VALUES(?,?,?)', [newUser._username, newUser._password, newUser._permission], (error) => {
+            if (error) {
+              console.error('Error:', error);
+              return res.status(500).json({ error: 'Internal server error.' });
+            }
+      
+            // Send a response
+            res.json({ message: 'User signed up successfully!' });
+        });
+    });
+});
 
 app.listen(8081, ()=> {
     console.log("listening");
