615c818c7b5a6a99f37d4f1ca99c8d0e596b4455
PeekMe01 <ralphdaher6@gmail.com>
2023-06-25 11:45:02 +0300

    Update server.js

--- back-end/server.js
+++ back-end/server.js
@@ -87,14 +87,109 @@ app.post('/login', (req,res) => {
                 password,
                 permission
             };
-            res.json({ message: 'User logged in successfully!', userData });
+            res.json({ message: 'User logged in successfully!' });
         }
     });
 });
 
-// admin sending results part
+// admin sending results part / or anyone with permission
 app.post('/sendresults', (req,res)=>{
+    const { username, password, send_to, result_context } = req.body;
+    const sendresults = 'INSERT INTO test_results(send_by, send_to, result_text, test_result_users_id) VALUES (?,?,?,?);';
+    const checkSendTo = 'SELECT id FROM test_result_users WHERE username = ?;';
+    const checkPermission = 'SELECT permission FROM test_result_users WHERE username = ? AND password = ?;';
+    var send_to_id;
+
+    if(!username, !password) { // this should NEVER be reached
+        return res.status(500).json({ error: 'How did we get here 💀.' });
+    }
+
+    if(!send_to, !result_context) {
+        return res.status(400).json({ error: 'All fields are required.' });
+    }
+
+    // let's make sure the sender has permission ( even tho he can't access the form without permission but we must stay protected no matter what )
+    db.query(checkPermission, [username, password], (error, results) => {
+        if (error) {
+            console.error('Error:', error);
+            return res.status(500).json({ error: 'Internal server error.' });
+        }
+
+        if(results.length == 0 ){
+            return res.status(403).json({ error: 'Nice hidden talent! Keep it hidden.' });
+        }
+
+        if(results.length == 1 ){
+            if(results[0].permission==false){
+                return res.status(403).json({ error: 'Nice try! Don\'t try it again ;) ' });
+            }
+        }
+    });
+
+    // now let's make sure the user we send to is a real user
+    db.query(checkSendTo, [send_to], (error, results) => {
+        if (error) {
+            console.error('Error:', error);
+            return res.status(500).json({ error: 'Internal server error.' });
+        }
+        if(results.length == 0){
+            return res.status(401).json({ error: 'User doesn\'t exist.' });
+        }
+
+        if(results.length==1){
+            send_to_id = results[0].id; // we now have successfully gotten the send_to user id
+        }
+
+        // we did this query inside because we want the "checkSendTo" query one to finish first
+        //now that we have everything we can send it
+        db.query(sendresults, [username, send_to, result_context, send_to_id], (error, results) =>{
+            if (error) {
+                console.error('Error:', error);
+                return res.status(500).json({ error: 'Internal server error.' });
+            }
+
+            // Send a response
+            res.json({ message: 'Result succesfully sent to ' + send_to });
+        });
+    });
+});
+
+// user retreiving his results
+app.post('/getresults', (req,res) => {
+    const { id, username, password} = req.body;
+    const getresults = 'SELECT send_by, result_text FROM test_results WHERE test_result_users_id = ?';
+    const checkUser = 'SELECT * FROM test_result_users WHERE id = ? AND username = ? AND password = ?';
+
+    if(!id, !username, !password) {
+        return res.status(400).json({ error: 'All fields are required.' });
+    }
+
+    // make sure these id, username and password are correct because we have trust issues with users
+    db.query(checkUser, [id,username,password], (error,results) =>{
+        if (error) {
+            console.error('Error:', error);
+            return res.status(500).json({ error: 'Internal server error.' });
+        }
+
+        if(results.length == 0){
+            return res.status(401).json({ error: 'User doesn\'t exist.' });
+        }
+
+        // if user exists we continue
+        db.query(getresults, [id], (error, results) => {
+            if (error) {
+                console.error('Error:', error);
+                return res.status(500).json({ error: 'Internal server error.' });
+            }
+            if(results.length == 0){
+                return res.status(404).json({ error: 'You have no results.' });
+            }
     
+            if(results.length>0){
+                res.json({ message: 'result Received results ' , results});
+            }
+        });
+    });
 });
 
 app.listen(8081, ()=> {
